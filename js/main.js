3c08c4a72da1643165074470b9a239e87d3d81a1
kiettran <59279640+kietanhtran@users.noreply.github.com>
2022-04-26 15:11:19 +0700

    Update color

--- js/main.js
+++ js/main.js
@@ -240,27 +240,27 @@ function changeCSS(cssFile, cssLinkIndex) {
 	document.getElementsByTagName("head").item(0).replaceChild(newlink, oldlink);
 }
 // disable right click
-// document.addEventListener('contextmenu', event => event.preventDefault());
+document.addEventListener('contextmenu', event => event.preventDefault());
  
-//     document.onkeydown = function (e) {
+    document.onkeydown = function (e) {
  
-//         // disable F12 key
-//         if(e.keyCode == 123) {
-//             return false;
-//         }
+        // disable F12 key
+        if(e.keyCode == 123) {
+            return false;
+        }
  
-//         // disable I key
-//         if(e.ctrlKey && e.shiftKey && e.keyCode == 73){
-//             return false;
-//         }
+        // disable I key
+        if(e.ctrlKey && e.shiftKey && e.keyCode == 73){
+            return false;
+        }
  
-//         // disable J key
-//         if(e.ctrlKey && e.shiftKey && e.keyCode == 74) {
-//             return false;
-//         }
+        // disable J key
+        if(e.ctrlKey && e.shiftKey && e.keyCode == 74) {
+            return false;
+        }
  
-//         // disable U key
-//         if(e.ctrlKey && e.keyCode == 85) {
-//             return false;
-//         }
-//     }
+        // disable U key
+        if(e.ctrlKey && e.keyCode == 85) {
+            return false;
+        }
+    }
